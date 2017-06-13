00fe9fe908ce98a0b2adf95dd5dbd4c21492424e
Nazar Marchuk <marcusn90@gmail.com>
2017-06-13 23:06:02 +0300

    read file, config format experiments

--- index.js
+++ index.js
@@ -3,13 +3,39 @@
 const commander = require('commander');
 const chalk = require('chalk');
 
+const fs = require('fs');
+
 const defaults = {
-    configFile: 'gnrtr.json'
+    configFile: 'gnrtr.config.json'
 };
 
 commander
     .option('-c, --config <filename>', 'Specify config file')
     .parse(process.argv);
 
-let configFile = commander.config || defaults.config;
+let configFile = commander.config || defaults.configFile;
 console.log(`Config file: ${chalk.bold.yellow( configFile )}`);
+
+fs.open(configFile, 'r', ( err, fd ) => {
+    if ( err ) {
+        if ( err.code === 'ENOENT' ) {
+            console.log(chalk.red(`File ${configFile} does not exist`));
+            return;
+        }
+
+        throw err;
+    }
+
+    fs.readFile(fd, 'utf-8', ( err, data ) => {
+        if ( err ) {
+            fs.close(fd);
+
+            throw err;
+        }
+
+        console.log(chalk.green('File content:'));
+        console.log(data);
+
+        fs.close(fd);
+    });
+})
